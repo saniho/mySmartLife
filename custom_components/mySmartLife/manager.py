a4ca73dc7f92f6091779e4fe443ea9dc06c394ea
saniho <sanihogithub@gmail.com>
2024-02-08 22:47:03 +0100

    first commit

--- custom_components/mySmartLife/manager.py
+++ custom_components/mySmartLife/manager.py
@@ -3,12 +3,12 @@ def call_message(msg):
     import datetime
     #print(f"---\nexample(1) receive: {msg}")
     msgJson = json.loads(msg)
+    if msgJson.get("devId") is not None:
+        devId = msgJson.get("devId")
     if msgJson.get('productKey') is not None:
         productKey = msgJson.get('productKey')
         if (productKey == "qgwcxxws"):  # switch
-            print("oupi")
             if ( msgJson.get("status")):
                 statusJson = msgJson.get("status")
-                print(datetime.datetime.now())
-                print(statusJson[0])
+                print(datetime.datetime.now(), "//", devId, statusJson[0])
 
--- testmySmartLife.py
+++ testmySmartLife.py
@@ -7,7 +7,6 @@ def testMySmartLife():
   import configparser
   mon_conteneur = configparser.ConfigParser()
   mon_conteneur.read("../myCredential/security.txt")
-  print( mon_conteneur.keys)
   accessId = mon_conteneur["SMARTLIFE"]['ACCESS_ID']
   accessKey = mon_conteneur["SMARTLIFE"]['ACCESS_KEY']
   mqEndPoint = mon_conteneur["SMARTLIFE"]['MQ_ENDPOINT']
