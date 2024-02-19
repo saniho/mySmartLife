a7f9880e414bf1e44e3297b449224783d1120414
saniho <sanihogithub@gmail.com>
2024-02-19 22:58:03 +0100

    cleaning add config_flow

---
+++ custom_components/mySmartLife/myEntitySmartLife.py
@@ -0,0 +1,72 @@
+
+import logging
+from collections import defaultdict
+
+from homeassistant.helpers.entity import Entity
+
+from homeassistant.const import (
+    ATTR_ATTRIBUTION,
+)
+from .const import (
+    DOMAIN,
+    __VERSION__,
+    __name__,
+)
+import datetime
+_LOGGER = logging.getLogger(__name__)
+class myEntitySmartLife(Entity):
+    """."""
+
+    def __init__(self, name, value):
+        """Initialize the sensor."""
+        self._name = name
+        self._timeout = 2 # seconde
+        self._attributes = {}
+        self._attributes.update(self.getDefaultStatusCounts())
+        self._state = value
+    def getDefaultStatusCounts(self):
+        status_counts = {}
+        status_counts["version"] = __VERSION__
+        status_counts["last_update"] = "%s"%(datetime.datetime.now())
+        return status_counts
+
+    @property
+    def unique_id(self):
+        "Return a unique_id for this entity."
+        return "mySmartLife.%s" %self._name
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return "mySmartLife.%s" %self._name
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement of this entity, if any."""
+        return ""
+    def _changeData(self, newValue ):
+        self._state = newValue
+        self._lastSynchro = datetime.datetime.now()
+    def _resetData(self):
+        self._state = "unknown"
+        self._update()
+    def _resetDataCall(self):
+        from threading import Timer
+        timer = Timer(self._timeout, self._resetData)
+        # Démarrage du Timer
+        timer.start()
+    def _update(self):
+        """Update device state."""
+        self._attributes = {}
+        self._attributes.update(self.getDefaultStatusCounts())
+        self.async_write_ha_state()
+    @property
+    def extra_state_attributes(self):
+        """Return the state attributes."""
+        return self._attributes
+    @property
+    def device_state_attributes(self):
+        """Return the state attributes."""
+        return self._attributes
---
+++ custom_components/mySmartLife/sensorMySmartLife.py
@@ -0,0 +1,67 @@
+import datetime
+import logging
+from collections import defaultdict
+
+try:
+    from .const import (
+        __VERSION__,
+        __name__,
+    )
+
+except ImportError:
+    from const import (
+        __VERSION__,
+        __name__,
+    )
+
+
+class manageSensorState:
+    def __init__(self):
+        self._myEarnApp = None
+        self._LOGGER = None
+        self.version = None
+        pass
+
+    def init(self, _myEarnApp, _LOGGER=None, version=None):
+        self._myEarnApp = _myEarnApp
+        if (_LOGGER == None):
+            _LOGGER = logging.getLogger(__name__)
+        self._LOGGER = _LOGGER
+        self.version = version
+
+    def getstatusMoney(self):
+        state = "unavailable"
+        status_counts = defaultdict(int)
+        status_counts["version"] = self.version
+
+        status_counts["version"] = __VERSION__
+        status_counts["amount"] = self._myEarnApp.getMoney()
+        self._attributes = status_counts
+        self._state = self._myEarnApp.getMoney()
+        return self._state, self._attributes
+
+    def getstatusTotalMoney(self):
+        state = "unavailable"
+        status_counts = defaultdict(int)
+        status_counts["version"] = self.version
+
+        status_counts["version"] = __VERSION__
+        status_counts["amount"] = self._myEarnApp.getTotalMoney()
+        self._attributes = status_counts
+        self._state = self._myEarnApp.getTotalMoney()
+        return self._state, self._attributes
+
+    def getstatusData(self):
+        state = "unavailable"
+        status_counts = defaultdict(int)
+        status_counts["version"] = self.version
+
+        status_counts["version"] = __VERSION__
+        status_counts["volume"] = self._myEarnApp.getData()
+        self._attributes = status_counts
+        self._state = self._myEarnApp.getData()
+        return self._state, self._attributes
+
+def logSensorState(status_counts):
+    for x in status_counts.keys():
+        print(" %s : %s" % (x, status_counts[x]))
